8ad6e52ebcfc6299e94e6a17fd7517914eea3bd2
Aidan van Geest <aidanvangeest@gmail.com>
2023-09-26 12:02:32 +0200

    Exercises 2.4 & 2.5

--- CourseInfo/src/Components/Course.jsx
+++ CourseInfo/src/Components/Course.jsx
@@ -2,14 +2,16 @@ import Content from "./Content"
 import Header from "./Header"
 import Total from "./Total"
 
-const Course = ({course}) => {
-    return (
-        <div>
-          <Header name={course.name} />
-          <Content parts={course.parts} />
-          <Total parts={course.parts} />
-        </div>
-      )
-}
+const Course = ({courses}) => (
+        courses.map(course => {
+            return (
+                <div key={course.id}>
+                <Header name={course.name} />
+                <Content parts={course.parts} />
+                <Total parts={course.parts} />
+                </div>
+            )
+        })
+    )
 
 export default Course;
